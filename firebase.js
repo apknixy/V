bcdaefdf03e7b97feb48e5dbf5776f0f0eb952fe
Apknixy <apknixy@gmail.com>
2025-06-14 17:05:05 +0530

    Update and rename home.html to firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDes3ioeygnWBqJB6NnHb7xj732JJhOqm8",
+  authDomain: "videofire-268ff.firebaseapp.com",
+  databaseURL: "https://videofire-268ff-default-rtdb.firebaseio.com",
+  projectId: "videofire-268ff",
+  storageBucket: "videofire-268ff.appspot.com",
+  messagingSenderId: "910374058927",
+  appId: "1:910374058927:web:6add3afdea07cfa2914d5a"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Firebase services
+const auth = firebase.auth();
+const db = firebase.database();
